76128a849b53cd200aa788de7cb8c2c4bd442b50
unknown <zuranimendoza@gmail.com>
2024-04-24 18:15:45 -0500

    Antes de debugear el error

--- Controllers/AsesoresController.cs
+++ Controllers/AsesoresController.cs
@@ -1,14 +1,14 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TurnoAgil.Data;
 using TurnoAgil.Models;
+using TurnoAgil.Data;
 
 namespace TurnoAgil.Controllers;
 
 public class AsesoresController : Controller
 {
-    private readonly MisericordiaContext _context;
+    public readonly MisericordiaContext _context;
     public AsesoresController(MisericordiaContext context)
     {
         _context = context;
@@ -27,12 +27,13 @@ public class AsesoresController : Controller
         return View();
     }
     [HttpPost]
-    public IActionResult Login(string nit, string password)
+    public IActionResult Login(Asesor model)
     {
-        var asesor = _context.Asesores.FirstOrDefault(e => e.NIT == nit && e.Password == password);
+        Console.WriteLine($"Este es el NIT: {model.NIT}");
+        var asesor = _context.Asesores.FirstOrDefault(e => e.NIT == model.NIT && e.Password == model.Password);
         if (asesor != null)
         {
-            // HttpContext.Session.SetString("AserorID", asesor.ID.ToString());
+            HttpContext.Session.SetString("AserorID", asesor.ID.ToString());
             HttpContext.Session.SetString("AsesorNombre", asesor.Nombre);
             HttpContext.Session.SetString("AsesorNIT", asesor.NIT);
             _context.SaveChanges();
